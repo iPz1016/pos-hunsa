788c22fdd38b9dbfdbed68707ad7dba0fde7c59e
1Pz <pee.nrm@gmail.com>
2023-12-09 23:49:11 +0700

    update denied.view

--- pos_hunsa/app/views/auth/denied.view.php
+++ pos_hunsa/app/views/auth/denied.view.php
@@ -1,12 +1,9 @@
 <?php require views_path('partials/header');?>
-
-	<br>
-		<div class="align-center my-3">
+		<div class="mt-7">
 			<img src='<?= WARNING_ICON ?>'width='150' draggable="false">
 			<h1>Access Denied!</h1>
 			<div><?=Auth::getMessage()?></div>
 			<a href="index.php?pg=login"><button class="btn btn-info my-2 w-25 py-4" style="font-size: 36px; font-weight: 700"> LOGIN</button></a>
 		</div>
-	<br>
 	
 <?php require views_path('partials/footer');?>
