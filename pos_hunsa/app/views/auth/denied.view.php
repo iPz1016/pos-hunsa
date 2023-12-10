0b610dfa158b1f9e4fb75dadd793847ce06b6da5
om2546 <chayarob.chan@gmail.com>
2023-12-10 10:02:01 +0700

    adjust auth denied page
    change redirect location if user logged in to home page and user did not log in to login page

--- pos_hunsa/app/views/auth/denied.view.php
+++ pos_hunsa/app/views/auth/denied.view.php
@@ -4,7 +4,13 @@
 			<img src='<?= WARNING_ICON ?>'width='150' draggable="false">
 			<h1>Access Denied!</h1>
 			<div><?=Auth::getMessage()?></div>
-			<a href="index.php?pg=login"><button class="btn btn-info my-2 w-25 py-4" style="font-size: 36px; font-weight: 700"> LOGIN</button></a>
+
+			<?php if(Auth::logged_in()): ?>
+				<a href="index.php?pg=home"><button class="btn btn-info my-2 w-25 py-4" style="font-size: 36px; font-weight: 700"> HOME</button></a>
+			<?php else: ?>
+				<a href="index.php?pg=login"><button class="btn btn-info my-2 w-25 py-4" style="font-size: 36px; font-weight: 700"> LOGIN</button></a>
+			<?php endif ?>
+
 		</div>
 	
 <?php require views_path('partials/footer');?>
