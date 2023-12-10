1264e4ac3b829e94c1eb5720ad247fbce93823c8
ddmoonyou <nuchpawee.peam@kmutt.ac.th>
2023-12-10 19:55:00 +0700

    Comment Admin folder

--- pos_hunsa/app/views/admin/btn-main.view.php
+++ pos_hunsa/app/views/admin/btn-main.view.php
@@ -1,8 +1,10 @@
             <div class="col-2 h5 m-2 text-center d-flex justify-content-start">
+			<!-- Back Button -->
 				<a href="index.php?pg=admin&tab=main" class="main-menu p-3 fw-bold">
 					<i class="fa fa-arrow-circle-left"></i> BACK
 				</a>
 			</div>
             <div class="col-md-7 m-2">
-				<h2><?= strtoupper($tab) ?></h2>
-			</div>
+			 <!-- Display the current tab as a heading -->
+			<h2><?= strtoupper($tab) ?></h2>
+		</div>
