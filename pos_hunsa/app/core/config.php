91252e7e068bff43401212b075af9ba6b793cabb
ddmoonyou <nuchpawee.peam@kmutt.ac.th>
2023-12-11 14:48:24 +0700

    Comment Core folder and index.php

--- pos_hunsa/app/core/config.php
+++ pos_hunsa/app/core/config.php
@@ -1,15 +1,20 @@
 <?php 
 
-
+// The name of application
 define("APP_NAME", "HUN-SA POS");
 
+// The time zone for application
 define("TIME_ZONE","Asia/Bangkok");
 
+// The path to the application logo image
 define("APP_LOGO","assets/images/logo/h-logo.png");
 
+// The path to the application logo with text image
 define("APP_LOGO_WITH_TEXT","assets/images/logo/hun-sa-pos-logo.png");
 
+// The path to the background image for the login page
 define("BACKGROUND_LOGIN","assets/images/background.png");
 
+// The path to the warning icon image
 define("WARNING_ICON","assets/images/warn.png");
 
--- pos_hunsa/app/core/model.php
+++ pos_hunsa/app/core/model.php
@@ -1,29 +1,29 @@
 <?php
 
-/**
- * model class
- */
+// Model class extending Database 
 class Model extends Database
 {
-
-
+	// Get the allowed columns based on the defined columns in the child class
+     	// @param array $data Data to filter
+     	// @return array Cleaned data with only allowed columns
 	protected function get_allowed_columns($data)
-	{
- 
+	{	
+		// Check if allowed columns are defined in the child class
  		if(!empty($this->allowed_columns)){
- 			
 			foreach ($data as $key => $value) {
-				// code...
+				// Remove any keys that are not in the allowed columns list
 				if(!in_array($key, $this->allowed_columns))
 				{
 					unset($data[$key]);
 				}
 			}
 		}
-
 		return $data;
  	}
 
+	// Insert a new record into the database
+     	// @param array $data Data to insert
+     	// @return void
 	public function insert($data)
 	{
 
@@ -39,12 +39,16 @@ class Model extends Database
 
 	}
 
+	// Update an existing record in the database
+	// @param $id Record identifier
+	// @param array $data Data to update
+	// @param string $key Name of the primary key column
+	// @return void
 	public function update($id,$data,$key="id")
 	{
 
 		$clean_array = $this->get_allowed_columns($data,$this->table);
 		$keys = array_keys($clean_array);
-		
 		$query = "update $this->table set ";
 
 		foreach ($keys as $column) {
@@ -61,6 +65,11 @@ class Model extends Database
 
 	}
 
+	
+	// Delete a record from the database based on the given identifier
+	// @param $id Record identifier
+	// @param string $id_name Name of the primary key column
+	// @return void
 	public function delete($id,$id_name="id")
 	{
 
@@ -72,15 +81,20 @@ class Model extends Database
 
 	}
 
+	// Retrieve records from the database based on specified conditions
+	// @param array $data Conditions for the WHERE clause
+	// @param int $limit Maximum number of records to retrieve
+	// @param int $offset Offset for paginating through records
+	// @param string $order Sorting order (asc or desc)
+	// @param string $order_column Column for sorting
+	// @return array Retrieved records
 	public function where($data,$limit = 10,$offset = 0,$order = "desc",$order_column = "id")
 	{
 
 		$keys = array_keys($data);
-		
 		$query = "select * from $this->table where ";
 
 		foreach ($keys as $key) {
-			// code...
 			$query .= "$key = :$key && ";
 		}
 
@@ -89,22 +103,27 @@ class Model extends Database
 
 		$db = new Database;
 		return $db->query($query,$data);	
-
 	}
 
+	// Retrieve all records from the database
+	// @param int $limit Maximum number of records to retrieve
+	// @param int $offset Offset for paginating through records
+	// @param string $order Sorting order (asc or desc)
+	// @param string $order_column Column for sorting
+	// @return array Retrieved records
 	public function getAll($limit = 10,$offset = 0,$order = "desc",$order_column = "id")
 	{
-
 		$query = "select * from $this->table order by $order_column $order limit $limit offset $offset";
   		
 		$db = new Database;
 		return $db->query($query);	
-
 	}
 
+	// Retrieve the first record from the database based on specified conditions
+	// @param array $data Conditions for the WHERE clause
+	// @return mixed|null First retrieved record or null if not found
 	public function first($data)
 	{
-
 		$keys = array_keys($data);
 		
 		$query = "select * from $this->table where ";
@@ -121,13 +140,6 @@ class Model extends Database
 		{
 			return $res[0];
 		}
-
 		return false;	
-
 	}
-
-	
-
 }
-
-
